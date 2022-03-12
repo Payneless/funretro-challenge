1a296a73182107e7773e43994f0f1e80564a5775
Jake Payne <paynekid96@gmail.com>
2022-03-11 21:16:30 -0700

    finish initial

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const { chromium } = require("playwright");
 const { exit } = require("process");
 
 const [url, file] = process.argv.slice(2);
-
+// Hoisted boardTitle so it can be used for filename
 let boardTitle = "";
+
 if (!url) {
   throw "Please provide a URL as the first argument.";
 }
 
+// Helps retrieve innerText from selected HTML element with .$eval
 const innerText = (node) => node.innerText.trim();
+// Main function for parsing and reformatting the data
 async function run() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
@@ -23,18 +26,20 @@ async function run() {
   if (!boardTitle) {
     throw "Board title does not exist. Please check if provided URL is correct.";
   }
-
+  //   Final returned text variable
   let parsedText = "";
+  // temp object setup for reference to notes for each column from previous board
+  let temp = {};
 
   const columns = await page.$$(".easy-card-list");
 
   for (let c = 0; c < columns.length; c++) {
     const columnTitle = await columns[c].$eval(".column-header", innerText);
+    temp[columnTitle] = [];
 
     const messages = await columns[c].$$(".easy-board-front");
-    if (messages.length) {
-      parsedText += columnTitle + "\n";
-    }
+    parsedText +=
+      c < columns.length - 1 ? columnTitle + "," : columnTitle + "\n";
     for (let m = 0; m < messages.length; m++) {
       const messageText = await messages[m].$eval(
         ".easy-card-main .easy-card-main-content .text",
@@ -44,20 +49,47 @@ async function run() {
         ".easy-card-votes-container .easy-badge-votes",
         innerText
       );
-      parsedText += `- ${messageText} (${votes})` + "\n";
+      parseInt(votes) > 0 ? temp[columnTitle].push(`${messageText}`) : null;
     }
-
-    if (messages.length) {
-      parsedText += "\n";
+  }
+  //   Reference array established from reference object setup earlier
+  const states = Object.keys(temp);
+  //   Used to track longest column length so it can format the notes properly in the output file
+  let mostNotes = 0;
+  //   used to set the max length of one column's notes
+  for (const key in temp) {
+    if (temp[key].length > mostNotes) {
+      mostNotes = temp[key].length;
+    }
+  }
+  //   outside loop for adding that row of notes to each column
+  for (let n = 0; n < mostNotes; n++) {
+    //   inside loop for looping through each column and adding the first note to each column
+    for (let k = 0; k < states.length; k++) {
+      // checks to see if a note exists at that index in the array for that object key ( references temp object)
+      if (temp[states[k]][n]) {
+        //   denotes when to format a new line
+        if (k === states.length - 1) {
+          parsedText += `${temp[states[k]][n]}\n`;
+        }
+        // only adds comma at end since it is not the end of the row
+        else {
+          parsedText += `${temp[states[k]][n]}, `;
+        }
+      }
+      //   adds a blank space to denote no notes of >1 vote for this column exist
+      else {
+        parsedText += " ,";
+      }
     }
   }
 
   return parsedText;
 }
-
-function writeToFile(filePath, data) {
+// added format argument to allow user to input their own file format if they want soemthing other than a csv output
+function writeToFile(filePath, data, format = "csv") {
   const resolvedPath = path.resolve(
-    filePath || `../${boardTitle.replace(/\s/g, "")}.csv`
+    filePath || `../${boardTitle.replace(/\s/g, "")}.${format}`
   );
   fs.writeFile(resolvedPath, data, (error) => {
     if (error) {
